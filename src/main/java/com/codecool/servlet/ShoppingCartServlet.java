784d04247c619bfd8db4f702424ec5ca740c1cda
tamasPetki <tonertop@gmail.com>
2018-07-07 19:48:42 +0200

    removed unnecessarys

--- src/main/java/com/codecool/servlet/ShoppingCartServlet.java
+++ src/main/java/com/codecool/servlet/ShoppingCartServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet(name = "ShoppingCartServlet", urlPatterns = {"/cart"}, loadOnStartup = 2)
 public class ShoppingCartServlet extends HttpServlet {
@@ -24,17 +22,5 @@ public class ShoppingCartServlet extends HttpServlet {
         }
 
         out.println("<html>\n" + "<head><title>SHOPPING CART</title></head>\n" + "<body>\n" + htmlCode + "<br><a href=\"/\">BACK TO ITEMS</a>" + "<body></html>");
-//        out.println(
-//                "<html>\n" +
-//                        "<head><title>Another page</title></head>\n" +
-//                        "<body>\n" +
-//                        "<h1>Hello CodeCooler!</h1>" +
-//                        (linkId == null ?
-//                                "<h3>No link was pressed</h3>" :
-//                                "<h3>Link " + linkId + " was pressed!</h3>") +
-//                        "<br/>" +
-//                        "<div>" + buffer.toString() + "</div>" +
-//                        "</body></html>"
-//        );
     }
 }
